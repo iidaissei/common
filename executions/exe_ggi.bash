e0747ffcbfffcf6968c719aafd603fc1df8fc546
iidaissei <issei.288.higashi@icloud.com>
2020-02-19 20:50:01 +0900

    2020/02/19

--- executions/exe_ggi.bash
+++ executions/exe_ggi.bash
@@ -7,5 +7,8 @@ gnome-terminal \
     $tab 'roscore'\
     $tab 'bash -c "sleep 1.0 ; ./minimal.launch.bash"'\
     $tab 'bash -c "sleep 1.2 ; ./3dsensor.launch.bash"'\
-    $tab 'bash -c "sleep 1.2 ; ./ggi_launcher.sh"'\
-    $tab 'bash -c "sleep 1.4 ; ./ggi_common.launch.bash"'\
+    $tab 'bash -c "sleep 1.4 ; ./rs_rgbd.launch.bash"'\
+    $tab 'bash -c "sleep 1.5 ; ./gmapping.launch.bash"'\
+    $tab 'bash -c "sleep 1.6 ; ./view_navigation.launch.bash"'\
+    $tab 'bash -c "sleep 1.7 ; ./ggi_launcher.sh"'\
+    $tab 'bash -c "sleep 1.7 ; ./ggi_common.launch.bash"'\
