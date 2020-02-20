97d2a7f48c82deaff3ef9ddbfed8d15b97d36d0c
KIT <KIT.RoboCup.HomePJ@gmail.com>
2020-02-20 16:54:28 +0900

    2020/02/20

--- executions/exe_ggi.bash
+++ executions/exe_ggi.bash
@@ -7,8 +7,9 @@ gnome-terminal \
     $tab 'roscore'\
     $tab 'bash -c "sleep 1.0 ; ./minimal.launch.bash"'\
     $tab 'bash -c "sleep 1.2 ; ./3dsensor.launch.bash"'\
-    $tab 'bash -c "sleep 1.4 ; ./rs_rgbd.launch.bash"'\
-    $tab 'bash -c "sleep 1.5 ; ./gmapping.launch.bash"'\
-    $tab 'bash -c "sleep 1.6 ; ./view_navigation.launch.bash"'\
-    $tab 'bash -c "sleep 1.7 ; ./ggi_launcher.sh"'\
-    $tab 'bash -c "sleep 1.7 ; ./ggi_common.launch.bash"'\
+    $tab 'bash -c "sleep 1.5 ; ./rs_rgbd.launch.bash"'\
+    $tab 'bash -c "sleep 1.8 ; ./ggi_launcher.sh"'\
+    $tab 'bash -c "sleep 1.8 ; ./ggi_common.launch.bash"'\
+    # $tab 'bash -c "sleep 1.7 ; ./gmapping.launch.bash"'\
+    # $tab 'bash -c "sleep 1.8 ; ./view_navigation.launch.bash"'\
+ 
